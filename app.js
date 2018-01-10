deb5020df0b615ac4c45397fe8ffaecd5f3699ac
Chuiyi Lin <chuiyilin@gmail.com>
2018-01-10 17:35:44 +0800

    Bug fix

--- app.js
+++ app.js
@@ -41,24 +41,24 @@ app.use('/files', express.static(process.cwd(), {
 app.get('/scan', function(req, res) {
     res.send(tree);
 });
-app.use('/files/' + folder_path_reverse + , express.static(folder_path));
+app.use('/files/' + folder_path_reverse, express.static(folder_path));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
